bac3304180845bfbe918279a20c9797f4e54e32f
acoderwannabe19 <acoderwannabe19>
2024-05-11 23:42:12 +0000

    Adding the price forecasting model

--- Predictive_Analysis/app.py
+++ Predictive_Analysis/app.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from joblib import load
 import pandas as pd
 import uvicorn
+from tensorflow.keras.models import load_model
 
 app = FastAPI()
 
@@ -17,6 +18,7 @@ app.add_middleware(
 
 NON_CUSTOM_MODEL = load("models/non_custom_prediction_pipeline.joblib")
 CUSTOM_MODEL = load("models/custom_prediction_pipeline.joblib")
+PRICE_FORECASTING_MODEL = load_model("models/price_forecasting_model.h5")
 
 
 @app.post("/api/v1")
@@ -54,6 +56,23 @@ async def on_message(request: Request):
             "status": "OK"
         }  
 
+
+@app.post("/api/price_forecast")
+async def on_message(request: Request):
+    try:
+        body = await request.json()
+        series = pd.DataFrame({"regions": [body["regions"]], "date": [body["date"]], "produits": [body["produits"]]})
+        prediction = PRICE_FORECASTING_MODEL.predict(series)  
+    except Exception as e:
+            return {
+            "status": "KO",
+            "error": repr(e)
+        }
+    else:
+        return {
+            "prix/KG": prediction[0],
+            "status": "OK"
+        } 
 
 if __name__=="__main__":
     uvicorn.run("app:app",host="0.0.0.0",port=5005)
